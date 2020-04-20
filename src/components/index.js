d9af6247e74546b24d3679091b81b32c9a6a9b7c
rever-whale <rever.whale@gmail.com>
2020-04-20 23:00:14 +0900

    plugin update

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,2 @@
-import Vue from "vue";
-import SimpleAlert from './SimpleAlert.vue';
-import Test from './Test.vue';
-
-const Components = {
-  SimpleAlert,
-  Test
-};
-
-Object.keys(Components).forEach(name => {
-  Vue.component(name, Components[name]);
-})
-
-export default Components;
+export { default as SimpleAlert } from './SimpleAlert.vue';
+export { default as Test } from './Test.vue';
---
+++ src/components/index2.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import SimpleAlert from './SimpleAlert.vue';
+import Test from './Test.vue';
+
+const Components = {
+  SimpleAlert,
+  Test
+};
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name]);
+})
+
+export default Components;
